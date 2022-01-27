0b3470fbe6b3df8e47366891d96e889dd0509c01
CheekatiDeepak <97727021+CheekatiDeepak@users.noreply.github.com>
2022-01-27 17:42:33 +0530

    Add files via upload

---
+++ DataType1.java
@@ -0,0 +1,14 @@
+
+public class DataType1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 10;
+		System.out.println("The integer value is: "+num);
+		String data = String.valueOf(num);
+		System.out.println("The string value is:"+data);
+		
+
+	}
+
+}
---
+++ DataType2.java
@@ -0,0 +1,14 @@
+
+public class DataType2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double num = 10.99;
+		System.out.println("The double  value:" + num);
+		int data = (int) num;
+		System.out.println("The integer value:" + data);
+		
+
+	}
+
+}
